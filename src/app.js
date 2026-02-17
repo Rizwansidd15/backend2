2685bd15ce962cc1bc85c0159d894f40fb6b13ab
Rizwansidd <siddrizwan518@gmail.com>
2026-02-17 09:28:15 +0530

    "url change"

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const authRoutes = require("./routes/auth.route");
 const foodRoutes = require('./routes/food.route')
 const cors = require('cors')
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://frontend-nu-orcin-43.vercel.app/',
     credentials: true,
 }));
 app.use(express.json());
