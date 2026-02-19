af4eba507435eae73adc487c9fc46967dc03333e
Rizwansidd <siddrizwan518@gmail.com>
2026-02-19 13:11:26 +0530

    "new update"

--- src/app.js
+++ src/app.js
@@ -4,18 +4,32 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.route");
-const foodRoutes = require('./routes/food.route')
-const cors = require('cors')
-app.use(cors({
-    origin: 'https://frontend-nu-orcin-43.vercel.app/',
+const foodRoutes = require("./routes/food.route");
+const cors = require("cors");
+const allowedOrigins = [
+  "https://frontend-nu-orcin-43.vercel.app",
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "http://localhost:3000",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-}));
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
-
-
 app.use("/api/auth", authRoutes);
-app.use("/api/food", foodRoutes)
+app.use("/api/food", foodRoutes);
 
 module.exports = app;
